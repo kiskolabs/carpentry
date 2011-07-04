97dbcfe172a5272e2ac71b72d81469616674949e
Joao Carlos <mail@joao-carlos.com>
2011-07-04 14:45:31 +0300

    Shows a 404 page when #consider_all_requests_local is false.

--- app/controllers/carpentry/prototypes_controller.rb
+++ app/controllers/carpentry/prototypes_controller.rb
@@ -6,6 +6,14 @@ module Carpentry
     def serve
       env["carpentry.prototype"] = true
       render "carpentry/prototypes/#{params[:file_path]}"
+    rescue ActionView::MissingTemplate
+      if Rails.application.config.consider_all_requests_local
+        raise
+      else
+        render :file => "#{Rails.root}/public/404.html",
+               :status => 404,
+               :layout => false
+      end
     end
   end
 end
---
+++ spec/integration/prototype_not_found_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe "No prototype found" do
+  context "when #consider_all_requests_local is set to false" do
+    it "responds with a 404 status code" do
+      Rails.application.config.stub(:consider_all_requests_local => false)
+
+      get "/p/i_do_not_exist"
+      response.status.should == 404
+    end
+  end
+
+  context "when #consider_all_requests_local is set to true" do
+    it "raises an ActionView::MissingTemplate exception" do
+      Rails.application.config.stub(:consider_all_requests_local => true)
+
+      expect {
+        get "/p/i_do_not_exist"
+      }.to raise_error(ActionView::MissingTemplate)
+    end
+  end
+end
