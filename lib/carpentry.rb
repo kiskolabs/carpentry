3d257d80d4ac8f686afa982c67fd6cfcbc8fe794
Virgil Mocanu <mocanuvirgil@gmail.com>
2015-04-13 12:11:51 +0300

    Removes no_robots_middleware requirement

--- lib/carpentry.rb
+++ lib/carpentry.rb
@@ -1,7 +1,6 @@
 # require "carpentry/no_robots_middleware"
 require "carpentry/engine"
 require "carpentry/rails/routes"
-require "carpentry/no_robots_middleware"
 
 module Carpentry
   class Engine < ::Rails::Engine
