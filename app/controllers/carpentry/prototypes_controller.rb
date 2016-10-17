776d3c59a4c0150b931ab6a6d206cd20167622ba
Vesa Vänskä <vesa@vesavanska.com>
2016-10-18 00:47:04 +0300

    Eliminates Rails deprecations

--- app/controllers/carpentry/prototypes_controller.rb
+++ app/controllers/carpentry/prototypes_controller.rb
@@ -1,10 +1,15 @@
 module Carpentry
   class PrototypesController < ApplicationController
-    before_filter :before_carpentry,
-      if: proc { respond_to?(:before_carpentry, true) }
+    if respond_to?(:before_action)
+      before_action :before_carpentry,
+        if: proc { respond_to?(:before_carpentry, true) }
+    else
+      before_filter :before_carpentry,
+        if: proc { respond_to?(:before_carpentry, true) }
+    end
 
     def serve
-      env["carpentry.prototype"] = true
+      request.env["carpentry.prototype"] = true
       render "carpentry/prototypes/#{params[:file_path]}"
     rescue ActionView::MissingTemplate
       if Rails.application.config.consider_all_requests_local
