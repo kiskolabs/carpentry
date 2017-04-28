e3e25503b454c4888be9997feee7f71c6fc4275d
Matias Korhonen <matias@kiskolabs.com>
2017-04-28 10:38:29 +0300

    Removes a deprecated config option
    RSpec::Core::Configuration#treat_symbols_as_metadata_keys_with_true_valu
    es= is deprecated, it is now set to true as default and setting it to
    false has no effect.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,6 @@ require "rspec/rails"
 require "capybara/rspec"
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
